93dc0adaf6b30dd900c2a0188cd3889b3b3df22d
cuban.quant <cuban.quant@gmail.com>
2020-10-25 22:45:27 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
     """Main entry point"""
     parser = argparse.ArgumentParser()
     parser.add_argument("--servers", help="Number of servers", type=int, default=1)
+    parser.add_argument("--file", help="File location", type=str, required=True)
     args = parser.parse_args()
     
